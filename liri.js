3ecb77b158e00b4fe31a6a5e20b59cb2cd85aabf
Peter Geyser <peterpgeyser@gmail.com>
2019-08-30 16:10:50 -0700

    Switch statements now functional

--- liri.js
+++ liri.js
@@ -2,4 +2,26 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+
+var input = process.argv;
+
+var cmd = input[2];
+var term = input.slice(3).join(" ");
+
+switch (cmd) {
+    case "concert-this":
+        console.log("Searching for " + term + "'s next show");
+        break;
+    case "spotify-this-song":
+        console.log("Searching for " + term + " on Spotify");
+        break;
+    case "movie-this":
+        console.log("Searching for " + term + " on IMDB");
+        break;
+    case "do-what-it-says":
+        console.log("Running the command from random.txt");
+        break;
+    default:
+        console.log("Not a valid command");
+}
