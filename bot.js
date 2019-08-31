4d1901b96d98147e08912780e191b5a697140d56
Peter Geyser <peterpgeyser@gmail.com>
2019-08-30 22:58:51 -0700

    do-what-it-says functionality complete

--- bot.js
+++ bot.js
@@ -1,25 +1,37 @@
+//Required modules
 require("dotenv").config();
 var axios = require("axios");
+var fs = require("fs");
 var keys = require("./keys");
 var moment = require("moment");
 var Spotify = require("node-spotify-api");
+
+//Create spotify object based off of node-spotify-api and using keys from keys.js/.env
 var spotify = new Spotify(keys.spotify);
 
+//Bot constructor that will be called in liri.js
 var BOT = function () {
+
+    //For appending in log.txt
     var divider = "\n====================================================================================\n\n"
 
+    //BOT Method that concert-this will use in liri.js
     this.findArtist = function (artist) {
+
+        //axios API call
         var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
         axios.get(URL).then(
             function (response) {
                 var json = response.data;
 
+                //Data that will be printed in console and log.txt
                 var data = [
                     "Venue name: " + json[0].venue.name,
                     "Venue location: " + json[0].venue.city + " " + json[0].venue.region,
                     "Date: " + moment(json[0].datetime).format("MM/DD/YYYY")
                 ].join("\n\n");
 
+                //Add data to log.txt and print to console
                 fs.appendFile("log.txt", "concert-this " + artist + "\n\n" + data + divider, function (err) {
                     if (err) throw err;
                     console.log(data);
@@ -28,7 +40,10 @@ var BOT = function () {
             })
     }
 
+    //BOT Method that spotify-this-song will use in liri.js
     this.findSong = function (song) {
+
+        //node-spotify-api call
         spotify
             .search({
                 type: 'track',
@@ -38,6 +53,7 @@ var BOT = function () {
             .then(function (response) {
                 var json = response.tracks.items[0];
 
+                //Data that will be printed in console and log.txt
                 var data = [
                     "Artist: " + json.artists[0].name,
                     "Song Name: " + json.name,
@@ -45,6 +61,7 @@ var BOT = function () {
                     "Preview: " + json.preview_url
                 ].join("\n\n");
 
+                //Add data to log.txt and print to console
                 fs.appendFile("log.txt", "spotify-this-song " + song + "\n\n" + data + divider, function (err) {
                     if (err) throw err;
                     console.log(data);
@@ -55,12 +72,16 @@ var BOT = function () {
             });
     }
 
+    //BOT Method that movie-this will use in liri.js
     this.findMovie = function (movie) {
+
+        //axios API call
         var URL = "https://www.omdbapi.com/?apikey=trilogy&t=" + movie + "&y&type=movie&plot=short";
         axios.get(URL).then(
             function (response) {
                 var json = response.data;
 
+                //Data that will be printed in console and log.txt
                 var data = [
                     "Title: " + json.Title,
                     "Released: " + json.Year,
@@ -72,6 +93,7 @@ var BOT = function () {
                     "Actors: " + json.Actors,
                 ].join("\n\n");
 
+                //Add data to log.txt and print to console
                 fs.appendFile("log.txt", "movie-this " + movie + "\n\n" + data + divider, function (err) {
                     if (err) throw err;
                     console.log(data);
@@ -80,4 +102,5 @@ var BOT = function () {
     }
 }
 
+//Make constructor available to liri.js
 module.exports = BOT;
--- liri.js
+++ liri.js
@@ -1,46 +1,85 @@
+//required modules
 var fs = require("fs");
 var BOT = require('./bot');
 
+//new BOT object from constructor in bot.js
 var bot = new BOT();
 
+//turning inputs from console into variables
 var input = process.argv;
 var cmd = input[2];
 var term = input.slice(3).join(" ");
 
-switch (cmd) {
-    case "concert-this":
-        console.log("\nSearching for " + term + "'s next show...\n");
-        bot.findArtist(term);
-        break;
-    case "spotify-this-song":
-        if (!term) {
-            console.log("\nSearching for The Sign by Ace of Base on Spotify...\n");
-            bot.findSong("The Sign Ace of Base");
-        } else {
-            console.log("\nSearching for " + term + " on Spotify...\n");
-            bot.findSong(term);
-        }
-        break;
-    case "movie-this":
-        if (!term) {
-            console.log("\nSearching for Mr. Nobody on OMDB...");
-            bot.findMovie("Mr. Nobody");
-        } else {
-            console.log("\nSearching for " + term + " on OMDB...");
-            bot.findMovie(term)
-        }
-        break;
-    case "do-what-it-says":
-        console.log("\nDoing what it says (what even is 'it?')....\n");
-        doingIt();
-        break;
-    default:
-        console.log("Not a valid command");
+//running main function
+liri(cmd, term);
+
+//switch case function that is called above and in doingIt
+function liri(cmd, term) {
+    switch (cmd) {
+
+        //concert-this case
+        case "concert-this":
+
+            //searching with user input
+            console.log("\nSearching for " + term + "'s next show...\n");
+            bot.findArtist(term);
+            break;
+
+        //spotify-this-song case
+        case "spotify-this-song":
+
+            //default search if no search terms
+            if (!term) {
+                console.log("\nSearching for The Sign by Ace of Base on Spotify...\n");
+                bot.findSong("The Sign Ace of Base");
+
+            //searching with user input
+            } else {
+                console.log("\nSearching for " + term + " on Spotify...\n");
+                bot.findSong(term);
+            }
+            break;
+
+        //movie-this case
+        case "movie-this":
+
+            //default search if no search terms
+            if (!term) {
+                console.log("\nSearching for Mr. Nobody on OMDB...\n");
+                bot.findMovie("Mr. Nobody");
+
+            //searching with user input
+            } else {
+                console.log("\nSearching for " + term + " on OMDB...\n");
+                bot.findMovie(term)
+            }
+            break;
+
+        //do-what-it-says case
+        case "do-what-it-says":
+
+            //calling separate doingIt function
+            console.log("\nDoing what it says (what even is 'it?')....");
+            doingIt();
+            break;
+
+        //in case user doesn't type anything, or types something incompatible
+        default:
+            console.log("Not a valid command");
+    }
 }
 
+//doingIt function called in do-what-it-says case
 function doingIt() {
+
+    //grab data from random.txt
     fs.readFile("./random.txt", "utf-8", function(err, data) {
         if (err) throw err;
-        console.log(data);
+        var randomTxt = data.toString().split(",");
+
+        //separate cmd and search term so that we can stick it back into liri function
+        var cmd = randomTxt[0];
+        var term = randomTxt[1].slice(1, -1);;
+        liri(cmd, term);
     })
 }
