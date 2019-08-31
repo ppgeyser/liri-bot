113dd551cb56ec9f1e8849920f93b2a661c2358e
Peter Geyser <peterpgeyser@gmail.com>
2019-08-30 21:37:40 -0700

    Spotify-this-song functionality complete

--- bot.js
+++ bot.js
@@ -6,15 +6,15 @@ var moment = require("moment");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-var BOT = function() {
+var BOT = function () {
     var divider = "\n====================================================================================\n\n"
 
-    this.findArtist = function(artist) {
+    this.findArtist = function (artist) {
         var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
         axios.get(URL).then(
-            function(response) {
+            function (response) {
                 var json = response.data;
-                
+
                 var data = [
                     "Venue name: " + json[0].venue.name,
                     "Venue location: " + json[0].venue.city + " " + json[0].venue.region,
@@ -29,34 +29,31 @@ var BOT = function() {
             })
     }
 
-    this.findSong = function(song) {
+    this.findSong = function (song) {
         spotify
-            .search({ type: 'track', query: song, limit: 1 })
-            .then(function(response) {
-              var json = response.tracks.items;
-              console.log(json);
+            .search({
+                type: 'track',
+                query: song,
+                limit: 1
             })
-            .catch(function(err) {
-              console.log(err);
+            .then(function (response) {
+                var json = response.tracks.items[0];
+
+                var data = [
+                    "Artist: " + json.artists[0].name,
+                    "Song Name: " + json.name,
+                    "Album: " + json.album.name,
+                    "Preview: " + json.preview_url
+                ].join("\n\n");
+
+                fs.appendFile("log.txt", "spotify-this-song " + song + "\n\n" + data + divider, function (err) {
+                    if (err) throw err;
+                    console.log(data);
+                })
+            })
+            .catch(function (err) {
+                console.log(err);
             });
-        
-        // var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-        // axios.get(URL).then(
-        //     function(response) {
-        //         var json = response.data;
-                
-        //         var data = [
-        //             "Venue name: " + json[0].venue.name,
-        //             "Venue location: " + json[0].venue.city + " " + json[0].venue.region,
-        //             "Date: " + moment(json[0].datetime).format("MM/DD/YYYY")
-        //         ].join("\n\n");
-
-        //         fs.appendFile("log.txt", "concert-this " + artist + "\n\n" + data + divider, function (err) {
-        //             if (err) throw err;
-        //             console.log(data);
-
-        //         })
-        //     })
     }
 }
 
